e4558be8bc6ae0fa38ce8c995a935d8c5ebf07c5
Brent-Dickinson <brenton.dickinson@gmail.com>
2026-02-15 21:16:44 -0500

    final remote-build.sh

--- remote-build.sh
+++ remote-build.sh
@@ -3,5 +3,11 @@ set -e
 LINUX=brent@192.168.1.228
 REMOTE=~/InfiniTime
 
-rsync -av --delete ./src/ "$LINUX:$REMOTE/src/"
-ssh -t brent@192.168.1.228 "cd ~/InfiniTime && sudo docker run --rm -v \$(pwd):/sources infinitime-build"
+# Sync source, but do NOT touch submodules under src/libs
+# Use -rt instead of -a to avoid permission/owner churn from /mnt/c.
+rsync -rtvi --delete \
+  --exclude 'libs/**' \
+  ./src/ "$LINUX:$REMOTE/src/"
+
+ssh -t "$LINUX" "cd $REMOTE && sudo docker run --rm -v \$(pwd):/sources infinitime-build"
+
